8092ee95f9ef1a5c8a7202baf8bebb4d932aac80
Marcus Kinsella <marcus.kinsella@gmail.com>
2014-10-04 13:58:53 -0700

    Add tests for node index labels.

--- tests/suffix_tree_tests.c
+++ tests/suffix_tree_tests.c
@@ -2,39 +2,123 @@
 #include "test_utils.h"
 #include "suffix_tree/suffix_tree.h"
 
+/* Test suffix tree construction on some random strings. */
 char* test_random_strings()
 {
-    const size_t str_len = 1000;
-    char* str = malloc(str_len * sizeof(char));
-    unsigned int i = 0;
+  const size_t str_len = 1000;
+  char* str = malloc(str_len * sizeof(char));
+  unsigned int i = 0;
 
-    for(i = 0; i < 5; i++) {
-        random_string(str, str_len);
-        SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
-        DBL_WORD rc = ST_SelfTest(stree);
+  for(i = 0; i < 5; i++) {
+    random_string(str, str_len);
+    SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
+    DBL_WORD rc = ST_SelfTest(stree);
 
-        mu_assert(rc == 1, "Suffix tree failed self test.");
+    mu_assert(rc == 1, "Suffix tree failed self test.");
 
-        ST_DeleteTree(stree);
-    }
+    ST_DeleteTree(stree);
+  }
 
-    free(str);
+  free(str);
 
-    return NULL;
+  return NULL;
 }
 
+/* Print a suffix tree. */
 char* test_print_stree()
 {
-    char str[] = "BANANA";
-    size_t str_len = sizeof(str) - 1;
+  char str[] = "BANANA";
+  size_t str_len = sizeof(str) - 1;
 
-    SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
-    printf("\nSuffix tree for BANANA:");
-    ST_PrintTree(stree);
+  SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
+  printf("\nSuffix tree for BANANA:");
+  ST_PrintTree(stree);
     
-    ST_DeleteTree(stree);    
+  ST_DeleteTree(stree);    
 
-    return NULL;
+  return NULL;
+}
+
+/* Helper function for testing node indices. */
+int label_test_dfs(const NODE* node, DBL_WORD** label_record, DBL_WORD num_nodes)
+{
+  DBL_WORD index = node->index;
+
+  if(index >= num_nodes) {
+    log_warn("Node has index value greater than the number of nodes.");
+    return 1;
+  }
+
+  if((*label_record)[index] != 0) {
+    log_warn("Node index %zu appears multiple times in suffix tree.", index);
+    return 1;
+  }
+
+  (*label_record)[index] = 1;
+
+  NODE* next_node = node->sons;
+  int ret = 0;
+  while(next_node != 0) {
+    ret = label_test_dfs(next_node, label_record, num_nodes);
+
+    if(ret != 0) return ret;
+
+    next_node = next_node->right_sibling;
+  }
+
+  return 0;
+}
+
+/* Test the index labels for a suffix tree for BANANA. */
+char* test_node_labels_banana()
+{
+  char str[] = "BANANA";
+  size_t str_len = sizeof(str) - 1;
+
+  SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
+  DBL_WORD* label_record = calloc(stree->num_nodes, sizeof(DBL_WORD));
+
+  int ret = label_test_dfs(stree->root, &label_record, stree->num_nodes);
+
+  mu_assert(ret == 0, "Failed node index verfication for BANANA.");
+  DBL_WORD i = 0;
+  for(i = 0; i < stree->num_nodes; i++) {
+    mu_assert(label_record[i] == 1, "Node index not visited during tour.");
+  }
+  
+  free(label_record);
+  ST_DeleteTree(stree);
+
+  return  NULL;
+}
+
+/* Test the index labels for a suffix tree for some random string. */
+char* test_node_labels_random()
+{
+  const size_t str_len = 10000;
+  char* str = malloc(str_len * sizeof(char));
+  unsigned int i = 0;
+
+  for(i = 0; i < 5; i++) {
+    random_string(str, str_len);
+    SUFFIX_TREE* stree = ST_CreateTree(str, str_len);
+    DBL_WORD* label_record = calloc(stree->num_nodes, sizeof(DBL_WORD));
+
+    int ret = label_test_dfs(stree->root, &label_record, stree->num_nodes);
+
+    mu_assert(ret == 0, "Failed node index verfication for random string.");
+    DBL_WORD i = 0;
+    for(i = 0; i < stree->num_nodes; i++) {
+      mu_assert(label_record[i] == 1, "Node index not visited during tour.");
+    }
+    
+    free(label_record);
+    ST_DeleteTree(stree);
+  }
+
+  free(str);
+
+  return NULL;
 }
 
 char* all_tests()
@@ -43,6 +127,8 @@ char* all_tests()
   
   mu_run_test(test_random_strings);
   mu_run_test(test_print_stree);
+  mu_run_test(test_node_labels_banana);
+  mu_run_test(test_node_labels_random);
 
   return NULL;
 }
