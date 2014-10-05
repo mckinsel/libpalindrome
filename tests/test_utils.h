cf1f83f6b93ec477e8619b4b52832057b37f54b8
Marcus Kinsella <marcus.kinsella@gmail.com>
2014-10-05 10:24:02 -0700

    Add function for creating a random array of size_ts.

--- tests/test_utils.h
+++ tests/test_utils.h
@@ -1,11 +1,19 @@
-
 #include <stdlib.h>
 
 void random_string(char* str, size_t str_len)
 {
-    static const char bases[] = "ACTG";
-    size_t i = 0;
-    for(i = 0; i < str_len; i++) {
-        str[i] = bases[rand() % (sizeof(bases) - 1)];
-    }
+  static const char bases[] = "ACTG";
+  size_t i = 0;
+  for(i = 0; i < str_len; i++) {
+    str[i] = bases[rand() % (sizeof(bases) - 1)];
+  }
+}
+
+void random_sizes(size_t* arr, size_t arr_len)
+{
+  static const size_t MAX_VAL = 10000;
+  size_t i = 0;
+  for(i = 0; i < arr_len; i++) {
+    arr[i] = rand() % MAX_VAL;
+  }
 }
