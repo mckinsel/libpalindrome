54ad7004843a646bf1deb95e74b5262d1bb54636
Marcus Kinsella <marcus.kinsella@gmail.com>
2014-12-06 17:24:23 -0800

    Add test for keeping track of PreviousStartItems.

--- tests/kk_eq_class_array_tests.c
+++ tests/kk_eq_class_array_tests.c
@@ -15,6 +15,10 @@ char* test_array_add()
   mu_assert(item->position == 0, "Incorrect position for LastItem.");
   mu_assert(item->next_run == NULL, "Incorrect NextRun for LastItem.");
   
+  EquivClassArray_add(array, 0, 4, str);
+  item = EquivClassArray_get_last_item(array, 0);
+  mu_assert(item->position == 4, "Incorrect position for LastItem.");
+
   EquivClassArray_delete(&array);
   return NULL;
 }
@@ -37,12 +41,34 @@ char* test_array_run()
   EquivClassArray_delete(&array);
   return NULL;
 }  
+
+
+char* test_previous_start_items()
+{
+  char str[] = "BANANA";
+  EquivClassArray_T array;
+  array = EquivClassArray_create(5);
+
+  EquivClassArray_add(array, 2, 1, str);
+  EquivClassArray_add(array, 2, 3, str);
+  EquivClassArray_add(array, 2, 4, str);
+  
+  EquivClassItem_T item = EquivClassArray_get_previous_start_item(array, 2);
+  mu_assert(item->position == 1, "Incorrect position for PreviousStartItem.");
+
+  EquivClassArray_set_previous_start_item(array, 2, item->next_item); 
+  item = EquivClassArray_get_previous_start_item(array, 2);
+  mu_assert(item->position == 3, "Incorrect position for PreviousStartItem.");
+  return NULL;
+}
+
 char* all_tests()
 {
   mu_suite_start();
   
   mu_run_test(test_array_add);
   mu_run_test(test_array_run);
+  mu_run_test(test_previous_start_items);
   return NULL;
 }
 
