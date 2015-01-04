36ab3a83b47e68407cf61e8ab7ec5c06a7f89eb1
Marcus Kinsella <marcus.kinsella@gmail.com>
2015-01-04 13:07:27 -0800

    Add a test that will trigger a realloc of the path from node to root.

--- tests/lca_lca_suffix_tree_alloc_tests.c
+++ tests/lca_lca_suffix_tree_alloc_tests.c
@@ -8,7 +8,6 @@ USE_FAILING_ALLOCS
 
 char* test_lca_suffix_tree_allocs()
 {
-  fprintf(stderr, "\n\nBEGIN LCASuffixTree_T ALLOC TESTS\n");
   char str[] = "BANANA";
   size_t str_len = sizeof(str) - 1;
   LCASuffixTree_T tree = NULL; 
@@ -24,7 +23,27 @@ char* test_lca_suffix_tree_allocs()
   
   mu_assert(ret_val == 0, "Failed final LCASuffixTree verification."); 
 
-  fprintf(stderr, "END LCASuffixTree_T ALLOC TESTS\n\n");
+  return NULL;
+}
+
+char* test_long_run_allocs()
+{
+  char str[] = "AAAAAAAAAAAAAAAAAAAAAAAAA";
+  size_t str_len = sizeof(str) - 1;
+
+  LCASuffixTree_T tree = NULL;
+  int i = 0;
+  int ret_val = 1;
+  for(i = 0; i < 250; i++){
+    tree = LCASuffixTree_create(str, str_len);
+    if(tree) {
+      ret_val = LCASuffixTree_verify(tree);
+    }
+    LCASuffixTree_delete(&tree);
+  }
+
+  mu_assert(ret_val == 0, "Failed final LCASuffixTree verification.");
+
   return NULL;
 }
 
@@ -32,8 +51,11 @@ char* all_tests()
 {
   mu_suite_start();
   
+  fprintf(stderr, "\n\nBEGIN LCASuffixTree_T ALLOC TESTS\n");
   mu_run_test(test_lca_suffix_tree_allocs);
-  
+  mu_run_test(test_long_run_allocs);
+  fprintf(stderr, "END LCASuffixTree_T ALLOC TESTS\n\n");
+
   FREE_FAILING_ALLOCS
 
   return NULL;
